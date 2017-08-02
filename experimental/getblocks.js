1f19bf12ece17f66364f10002451951ec6d901d2
Gawnie <gawnieg@gmail.com>
2017-08-02 09:29:53 +0100

    added disableStorage and disableMemory flags to make faster

--- experimental/getblocks.js
+++ experimental/getblocks.js
@@ -2,7 +2,7 @@ var Web3 = require('web3');
 var web3 = new Web3();
 
 try{
-  web3.setProvider(new web3.providers.HttpProvider('http://146.169.44.231:8545'));
+  web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 }
 catch(err){
 console.log("Chucking error" +err);
@@ -21,7 +21,7 @@ unrealres= getdata();
 function getdata(){
   var daycentres = web3.currentProvider.sendAsync({
     method: "debug_traceTransaction",
-    params: ["0x6c9b61353702df9c6a6073392a40d4746e0cddd7410bce0c56c5e1bad2217b7e",{}],  //  see docs, was ->params: ['0x272d5cfed972a35437833802595d170cd6288f2f7393d1d57af1a5955ab1dabf',{}]
+    params: ["0x272d5cfed972a35437833802595d170cd6288f2f7393d1d57af1a5955ab1dabf",{disableStorage: true, disableMemory:true}],  //  see docs, was ->params: ['0x272d5cfed972a35437833802595d170cd6288f2f7393d1d57af1a5955ab1dabf',{}]
     jsonrpc: "2.0",
     id:"2"},
       function(err,result){
